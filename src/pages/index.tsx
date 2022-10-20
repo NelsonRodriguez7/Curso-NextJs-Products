7b27f511547459781e0cda10ced0cbba4f1867fd
NelsonRodriguez7 <nelsonpay2020@hotmail.com>
2022-10-19 19:11:35 -0500

    Funcionamiento

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { ApplicationWrapper } from '../components/layout/ApplicationWrapper'
 
-
 type HomeProps = {
   response: string;
 };
